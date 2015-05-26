057b008b08d61c5f523ad22f743232e531c50d4b
Tyler Folkman <tylerfolkman@gmail.com>
2015-05-25 20:30:54 -0700

    Fixed static address

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import send_file
 from flask import render_template
 app = Flask(__name__)
-app._static_folder = "/Users/tylerfolkman/projects/dashboard/static" 
+app._static_folder = "/Users/tylerfolkman/projects/lwd_website/static"
 
 @app.route("/")
 def index():
