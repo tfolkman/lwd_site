db1e3ac4786abf2c2ab6bcc93e219357d6c3ca7c
Tyler Folkman <tylerfolkman@gmail.com>
2015-06-05 21:27:21 -0700

    RSS Reader and more social

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def index():
 def resources():
     return render_template("resources.html")
 
+@app.route("/posts.rss.xml/")
+def posts_rss():
+    return render_template("posts.rss.xml")
+
 if __name__ == "__main__":
     app.run(debug=True)
