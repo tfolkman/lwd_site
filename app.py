54ce29c21020399de54312328d13a67cc2db9a33
Tyler Folkman <tylerfolkman@gmail.com>
2015-06-16 22:01:35 -0700

    nfl rankings on main page

--- app.py
+++ app.py
@@ -16,46 +16,39 @@ def index():
 def resources():
     return render_template("resources.html")
 
-@app.route("/nfl/", methods=['GET', 'POST'])
-def nfl():
+@app.route("/nfl_historical_rankings/", methods=['GET', 'POST'])
+def nfl_historical_rankings():
 	import psycopg2
 
-	try:
-	    conn=psycopg2.connect("dbname=nfl_football user=deploy")
-	except:
-	    print "I am unable to connect to the database."
-	    
+	conn=psycopg2.connect("dbname=nfl_football user=deploy")    
 	cur = conn.cursor()
-	try:
-	    cur.execute("""select distinct team from off_regseason_gamestats order by team;""")
-	    rows = cur.fetchall()
-	    names = []
-	    for row in rows:
-	        names.append(row[0])
-	except:
-	    print "I can't drop our test database!"
+	cur.execute("""select distinct team from off_regseason_gamestats order by team;""")
+	teamNames = cur.fetchall()
+	teamNamesList = []
+	for name in teamNames:
+		teamNamesList.append(name[0])
 
 	if request.method == 'POST':
 		name=request.form['team']
 	else:
 		name="San Francisco 49ers"
 
-	cmd = "select rk, year from off_regseason_gamestats where team like (%s) order by year;"
-	cur.execute(cmd, (name, ))
-	rows = cur.fetchall()
-	rk = []
-	year = []
-	for row in rows:
-		rk.append(row[0])
-		year.append(row[1])
-	cmd = "select rk from def_rev_regseason_gamestats where team like (%s) order by year;"
-	cur.execute(cmd, (name, ))
-	rows = cur.fetchall()
-	d_rk = []
-	for row in rows:
-		d_rk.append(row[0])
+	offRankCmd = "select rk, year from off_regseason_gamestats where team like (%s) order by year;"
+	cur.execute(offRankCmd, (name, ))
+	offRankRows = cur.fetchall()
+	offRankList = []
+	yearList = []
+	for row in offRankRows:
+		offRankList.append(row[0])
+		yearList.append(row[1])
+	defRankCmd = "select rk from def_rev_regseason_gamestats where team like (%s) order by year;"
+	cur.execute(defRankCmd, (name, ))
+	defRankRows = cur.fetchall()
+	defRankList = []
+	for row in defRankRows:
+		defRankList.append(row[0])
 
-	return render_template("historical_nfl_rankings.html", team_names=names, name=name, o_data_years=year, o_data_rk=rk, d_data_rk=d_rk)
+	return render_template("historical_nfl_rankings.html", team_names=teamNamesList, name=name, o_data_years=yearList, o_data_rk=offRankList, d_data_rk=defRankList)
 
 @app.errorhandler(404)
 def error_handler(error):
