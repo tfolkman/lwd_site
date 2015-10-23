65493cae032a5f2974618cd2b8c5f89320c1b43d
Tyler Folkman <tylerfolkman@gmail.com>
2015-10-23 15:33:44 -0500

    Have things good now; look at adding redis and then most recent post

---
+++ lwd/nfl_views.py
@@ -0,0 +1,36 @@
+from flask import render_template, request
+from . import app, db
+from sqlalchemy import text
+
+
+@app.route("/nfl_historical_rankings/", methods=['GET', 'POST'])
+def nfl_historical_rankings():
+
+    if request.method == 'POST':
+        name = request.form['team']
+    else:
+        name = "San Francisco 49ers"
+
+    team_names = db.engine.execute("""select distinct team from off_regseason_gamestats order by team;""")
+    team_names_list = []
+    for team_name in team_names:
+        team_names_list.append(team_name[0])
+
+    team_data = db.engine.execute(text("""select o.year, o.rk as off_rk, d.rk as def_rk
+                            from off_regseason_gamestats o
+                            left join def_rev_regseason_gamestats d
+                              on o.year = d.year and o.team = d.team
+                            where o.team like :name
+                            order by year;"""), {'name': "San Francisco 49ers"})
+    off_rank_list = []
+    year_list = []
+    def_rank_list = []
+    for row in team_data:
+        off_rank_list.append(row[1])
+        year_list.append(row[0])
+        def_rank_list.append(row[2])
+
+    return render_template("/nfl/historical_nfl_rankings.html", team_names=team_names_list, name=name,
+                           o_data_years=year_list,
+                           o_data_rk=off_rank_list,
+                           d_data_rk=def_rank_list)
--- lwd/views.py
+++ lwd/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify
 from flask import request
 from flask import send_from_directory
 from . import app, db
@@ -104,42 +104,6 @@ def resources():
 @app.route("/rss_feed.xml")
 def feed():
     return send_from_directory(app.static_folder, request.path[1:])
-
-
-@app.route("/nfl_historical_rankings/", methods=['GET', 'POST'])
-def nfl_historical_rankings():
-    import psycopg2
-
-    conn = psycopg2.connect("dbname=nfl_football user=deploy")
-    cur = conn.cursor()
-    cur.execute("""select distinct team from off_regseason_gamestats order by team;""")
-    teamNames = cur.fetchall()
-    teamNamesList = []
-    for name in teamNames:
-        teamNamesList.append(name[0])
-
-    if request.method == 'POST':
-        name = request.form['team']
-    else:
-        name = "San Francisco 49ers"
-
-    offRankCmd = "select rk, year from off_regseason_gamestats where team like (%s) order by year;"
-    cur.execute(offRankCmd, (name,))
-    offRankRows = cur.fetchall()
-    offRankList = []
-    yearList = []
-    for row in offRankRows:
-        offRankList.append(row[0])
-        yearList.append(row[1])
-    defRankCmd = "select rk from def_rev_regseason_gamestats where team like (%s) order by year;"
-    cur.execute(defRankCmd, (name,))
-    defRankRows = cur.fetchall()
-    defRankList = []
-    for row in defRankRows:
-        defRankList.append(row[0])
-
-    return render_template("historical_nfl_rankings.html", team_names=teamNamesList, name=name, o_data_years=yearList,
-                           o_data_rk=offRankList, d_data_rk=defRankList)
 
 
 @app.route('/robots.txt')
