e5a8159846ec1d5abb99d333d58350aa6f1863d8
Tyler Folkman <tylerfolkman@gmail.com>
2016-02-28 10:30:32 -0600

    working towards data for lstm

--- lwd/pres_views.py
+++ lwd/pres_views.py
@@ -1,10 +1,12 @@
 from flask import render_template, request
 from . import app, db, redis_db
 import json
+from tables import open_file
 from sqlalchemy import text
 
 @app.route("/talk_like_pres_candidate/")
 def talk_like_pres_candidate():
+    start_samples = open_file("pres_start_samples.hdf", driver="H5FD_CORE").root.start_samples[:]
     sections = [
         {
             'href': '/',
