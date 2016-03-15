b1dd30229df5669c930c70d7e2b0b81435fe95bb
Tyler Folkman <tylerfolkman@gmail.com>
2016-03-14 20:53:12 -0500

    lstm finished

--- lwd/static/js/pres_cand/main.js
+++ lwd/static/js/pres_cand/main.js
@@ -41,7 +41,9 @@ var GenText = React.createClass({
             genText = <SuccessGenText gen_text={this.props.gen_text} />;
         }
         return (
-            genText
+            <p>
+            {genText}
+            </p>
         );
     }    
 })
@@ -101,6 +103,7 @@ var PresApp = React.createClass({
         return (
             <div className="container">
                 <h1 className="text-left bold-h1">Talk Like a Presidential Candidate</h1>
+                <h2 className="text-left bold-h2">An LSTM Language Model</h2>
                 <p className="line"></p>
                 <div className="row">
                     <div className="col-sm-4">
@@ -120,7 +123,21 @@ var PresApp = React.createClass({
                     </div>
                 </div>
                 <p className="line"></p>
+                <h2 className="bold-h3">generated text:</h2>
                 <GenText gen_text={this.state.gen_text} error_flag={this.state.gen_error}/>
+                <p className="line"></p>
+                <p className="bold-p">
+                    I have been wanting to try my hand at an LSTM model for a while now. If you are not familiar with LSTM networks, I would highly recommend <a href="http://colah.github.io/posts/2015-08-Understanding-LSTMs/">this introduction</a>. One thing this model can be used for is a language model. The goal of a language model is to predict the next word given a number of previous words. For example, given "I will win the" what is a probable next word? Perhaps race? 
+                </p>
+                <p className="bold-p"> 
+                    I decided it would be interesting to try and develop a language model that speaks like a Presidential candidate. To do so, I scraped the transcipts from some presidential (republican and democratic) debates. The code for this can be found <a href="https://github.com/tfolkman/learningwithdata/blob/master/Get_Debate_Data.ipynb">here.</a> To be honest, I didn't spend much time confirming an exact scrap of the data and simply split sentences on spaces to get words.
+                </p>
+                <p className="bold-p"> 
+                    To build the model, I used <a href="http://keras.io/">Keras.</a> This is my first time using Keras and I found it to be pretty nice. It was a small amount of code to build my model and was easy to experiment with. The code used to build my model can be found <a href="https://github.com/tfolkman/learningwithdata/blob/master/LSTM_Debate_Model.ipynb">here.</a> I built the model to predict a word given the 5 previous words. You can then chain this together to create sentences. For example, seeding the model with "START I want to be" might predict the next word as "the." Note: START is a special token I use to signify the beginning of a sentence. END is also a special token to specify the end of a sentence. Then using, "I want to be the", perhaps my model would predict "President." Lastly, given, "want to be the President" my model could predict END. This would then be the end of a sentence - "I want to be the President."
+                </p>
+                <p className="bold-p"> 
+                    To let others experiment with this model, I developed the web app seen above. This app will produce a single sentence give 4 seed words (only 4 because the first is the START token). One can input any 4 words (space separated) and see what is produced. If you try using a word not in the vocabulary, an error will be returned. Or you can have some seed words generated for you. You can also specify a diversity value (the slider). Increasing the value, creates sentences with more diverse word selections. It should be noted that my model currently suffers from a lack of data and not a lot of training. I am working on fixing the latter. I wanted to make sure I could get this web app working first, and now that it is, I will upload some better weights once I do some more training. 
+                </p>
             </div>
         );
     }
--- lwd/static/js/pres_cand/pres_bundle.js
+++ lwd/static/js/pres_cand/pres_bundle.js
@@ -24657,7 +24657,11 @@ var GenText = React.createClass({
         } else {
             genText = React.createElement(SuccessGenText, { gen_text: this.props.gen_text });
         }
-        return genText;
+        return React.createElement(
+            'p',
+            null,
+            genText
+        );
     }
 });
 
@@ -24722,6 +24726,11 @@ var PresApp = React.createClass({
                 { className: 'text-left bold-h1' },
                 'Talk Like a Presidential Candidate'
             ),
+            React.createElement(
+                'h2',
+                { className: 'text-left bold-h2' },
+                'An LSTM Language Model'
+            ),
             React.createElement('p', { className: 'line' }),
             React.createElement(
                 'div',
@@ -24767,7 +24776,57 @@ var PresApp = React.createClass({
                 )
             ),
             React.createElement('p', { className: 'line' }),
-            React.createElement(GenText, { gen_text: this.state.gen_text, error_flag: this.state.gen_error })
+            React.createElement(
+                'h2',
+                { className: 'bold-h3' },
+                'generated text:'
+            ),
+            React.createElement(GenText, { gen_text: this.state.gen_text, error_flag: this.state.gen_error }),
+            React.createElement('p', { className: 'line' }),
+            React.createElement(
+                'p',
+                { className: 'bold-p' },
+                'I have been wanting to try my hand at an LSTM model for a while now. If you are not familiar with LSTM networks, I would highly recommend ',
+                React.createElement(
+                    'a',
+                    { href: 'http://colah.github.io/posts/2015-08-Understanding-LSTMs/' },
+                    'this introduction'
+                ),
+                '. One thing this model can be used for is a language model. The goal of a language model is to predict the next word given a number of previous words. For example, given "I will win the" what is a probable next word? Perhaps race?'
+            ),
+            React.createElement(
+                'p',
+                { className: 'bold-p' },
+                'I decided it would be interesting to try and develop a language model that speaks like a Presidential candidate. To do so, I scraped the transcipts from some presidential (republican and democratic) debates. The code for this can be found ',
+                React.createElement(
+                    'a',
+                    { href: 'https://github.com/tfolkman/learningwithdata/blob/master/Get_Debate_Data.ipynb' },
+                    'here.'
+                ),
+                ' To be honest, I didn\'t spend much time confirming an exact scrap of the data and simply split sentences on spaces to get words.'
+            ),
+            React.createElement(
+                'p',
+                { className: 'bold-p' },
+                'To build the model, I used ',
+                React.createElement(
+                    'a',
+                    { href: 'http://keras.io/' },
+                    'Keras.'
+                ),
+                ' This is my first time using Keras and I found it to be pretty nice. It was a small amount of code to build my model and was easy to experiment with. The code used to build my model can be found ',
+                React.createElement(
+                    'a',
+                    { href: 'https://github.com/tfolkman/learningwithdata/blob/master/LSTM_Debate_Model.ipynb' },
+                    'here.'
+                ),
+                ' I built the model to predict a word given the 5 previous words. You can then chain this together to create sentences. For example, seeding the model with "START I want to be" might predict the next word as "the." Note: START is a special token I use to signify the beginning of a sentence. END is also a special token to specify the end of a sentence. Then using, "I want to be the", perhaps my model would predict "President." Lastly, given, "want to be the President" my model could predict END. This would then be the end of a sentence - "I want to be the President."'
+            ),
+            React.createElement(
+                'p',
+                { className: 'bold-p' },
+                'To let others experiment with this model, I developed the web app seen above. This app will produce a single sentence give 4 seed words (only 4 because the first is the START token). One can input any 4 words (space separated) and see what is produced. If you try using a word not in the vocabulary, an error will be returned. Or you can have some seed words generated for you. You can also specify a diversity value (the slider). Increasing the value, creates sentences with more diverse word selections. It should be noted that my model currently suffers from a lack of data and not a lot of training. I am working on fixing the latter. I wanted to make sure I could get this web app working first, and now that it is, I will upload some better weights once I do some more training.'
+            )
         );
     }
 });
